ff9a397ad913b38709303aeb18775ac7344d94e8
Rafael <rcarneiro782@gmail.com>
2022-06-05 08:17:52 -0300

    erro

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let totalSlides = document.querySelector('.slider-item').length;
 let currentSlide = 0;
 
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
 document.querySelector('.slider-width').style.width =
 `calc(100vw * ${totalSlides})`;
 document.querySelector('.slider-controls').style.height= 
@@ -17,13 +19,16 @@ function goPrev(){
 function goNext(){
   currentSlide++;
   if(currentSlide > (totalSlides-1)){
-      currentSlide = 0
+      currentSlide = 0;
   }
   updateMargin();
 }
 
 function updateMargin(){
-let newMargin = (currentSlide * document.body.clientWidth);
+  let sliderItemWidth = document.querySelector('.slider-item').clientWidth;
+   let newMargin = (currentSlide * sliderItemWidth);
 document.querySelector('.slider-width').style.marginLeft = 
    `-${newMargin}px`;
 }
+
+//setInterval(goNext, 2000);
